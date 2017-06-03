c610674b543f00af351f0b8791eb1e6a4f55f3b3
LEOnardo Ma <LEO@LEOnardos-MacBook-Pro.local>
2017-06-03 16:44:15 -0700

    first commit

---
+++ css/test.py
@@ -0,0 +1,13 @@
+import turtle;
+turt = turtle.Turtle();
+joe = turtle.Turtle();
+
+turt.goto(30, 30);
+
+joe.shape("turtle");
+joe.right(130);
+joe.penup();
+joe.forward(50);
+
+
+turtle.mainloop();
