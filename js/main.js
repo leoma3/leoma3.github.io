e1284f02805828845880672597a4e82d565d1d80
LEOnardo Ma <LEO@d142-058-219-089.wireless.sfu.ca>
2017-06-12 17:45:09 -0700

    photo swipe

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
-    $(document).on('click', 'a.from-left', function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 500);
+$(document).on('click', 'a.from-left', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
+
+$(document).ready(function() {
+    $("#welcome-ms").fadeIn(1000, function() {
+        $(this).animate({
+            width: "100%"
+        }, 1000, function() {
+            $(this).animate({
+                color: "black"
+            }, 1000)
+        });
     });
+
+
+});
