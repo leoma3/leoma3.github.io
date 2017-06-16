8be040ff0734154b028f47de644538e1a50b49c9
LEOnardo Ma <LEO@LEOnardos-MacBook-Pro.local>
2017-06-16 15:49:27 -0700

    add js for mobiles, ios & android

--- js/main.js
+++ js/main.js
@@ -3,18 +3,85 @@ $(document).on('click', 'a.from-left', function(event) {
     event.stopPropagation();
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
+    }, 1500);
 });
 
 $(document).ready(function() {
+    if (getMobileOperatingSystem()=="iOS") {
+        $("#self").css({
+            "background-color": "black",
+            "min-height": "50vh",
+            "background-position": "0 4.4em",
+            "margin-top": "3.2em"
+        });
+        $("#self-empty").css({
+        	"background-color": "white"
+        });
+        $("#self-normal").css({
+            "min-height": "50vh",
+            "background-position": "0px 92vh"
+        });
+
+        $("#uc-mockup").css({
+            "min-height": "50vh",
+            "background-position": "0px 357vh"
+        });
+        $("#data-vis").css({
+            "min-height": "50vh",
+            "background-position": "0px 500vh"
+        });
+        $("#eds-wiring").css({
+            "min-height": "50vh",
+            "background-position": "0px 874vh"
+        });
+    } else if (getMobileOperatingSystem()=="Android"){
+        $("#self").css({
+            "background-color": "black",
+            "min-height": "100vw",
+            "margin-top": "3.2em",
+            "background-size": "contain"
+        });
+    	$("#self-empty").css({
+        	"background-color": "white"
+        });
+        $("#self-normal, #uc-mockup").css({
+            "min-height": "100vw",
+            "background-size" : "contain"
+        });
+        $(".content-offset span").css({
+        	"top": "-4.5em"
+        });
+    }
+
+
     $("#welcome-ms").fadeIn(1000, function() {
         $(this).animate({
             width: "100%"
         }, 1000, function() {
-        	$(this).html("Welcome to my portfolio");
+            $(this).html("Welcome to my portfolio");
             $(this).animate({
                 color: "rgba(240, 240, 240, 1)"
             }, 1000);
         });
     });
 });
+
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+      // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+        return "Windows Phone";
+    }
+
+    if (/android/i.test(userAgent)) {
+        return "Android";
+    }
+
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "iOS";
+    }
+
+    return "unknown";
+}
